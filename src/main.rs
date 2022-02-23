9a43606fa68c10214be8c84e0586a5a2959d286e
takaknot <mail.takayu.k@gmail.com>
2022-02-24 07:28:08 +0900

    [wip:init] added mock server with tokio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
+
+
+async fn serve(_: usize) {
     println!("Hello, world!");
+}
+
+
+fn main() {
+    let mut handlers = Vec::new();
+
+    let h = std::thread::spawn(move || {
+        tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap()
+            .block_on(serve(0));
+    });
+
+    handlers.push(h);
+
+    for handler in handlers {
+        handler.join().unwrap();
+    }
 }
