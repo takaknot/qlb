51f714236c9f05faa54a4000f7c1577cd597959f
takaknot <mail.takayu.k@gmail.com>
2022-03-08 04:14:00 +0000

    [wip] added packet parser from MaybeUninit

--- qlb/src/main.rs
+++ qlb/src/main.rs
@@ -1,13 +1,33 @@
+#![feature(maybe_uninit_uninit_array, maybe_uninit_slice)]
 
-use std::io;
 use std::error::Error;
 use std::net::SocketAddr;
 use socket2::{Socket, Protocol, Domain, Type};
 
 use std::mem::MaybeUninit;
 
+pub const MAX_LEN: usize = 1024;
 
-async fn serve(i: usize) -> Result<(), io::Error> {
+fn parse_packet(buf: [MaybeUninit<u8>; MAX_LEN]) {
+
+    let pkt: [u8; MAX_LEN] = unsafe {
+        std::mem::transmute::<_, [u8; MAX_LEN]>(buf)
+    };
+
+    if pkt.len() < 21 {
+        return;
+    }
+    for i in 0..4 {
+        println!("{:?}", pkt[i]);
+    }
+
+    //let mut _x = Pin::new(&buf[i]).get_ref();
+    //let _y = buf.first_mut().unwrap();
+    //let _x = unsafe { &*buf.as_ptr() };
+    //let _y: u8 = _x.first_ptr();
+}
+
+async fn serve(i: usize) -> Result<(), std::io::Error> {
 
     let addr: SocketAddr = "0.0.0.0:4433".parse().unwrap();
 
@@ -24,16 +44,22 @@ async fn serve(i: usize) -> Result<(), io::Error> {
     
     sock.bind(&addr.into()).unwrap();
 
-    let send_buf: Vec<u8> = vec![128; 1024];
+    let send_buf: Vec<u8> = vec![97; MAX_LEN];
 
+    /*
     let mut recv_buf: [MaybeUninit<u8>; 1024] = unsafe {
-        MaybeUninit::uninit().assume_init()
+        //MaybeUninit::uninit().assume_init()
+        MaybeUninit::uninit_array()
     };
+    */
+    let mut recv_buf: [MaybeUninit<u8>; MAX_LEN] = MaybeUninit::uninit_array();
 
     loop {
         match sock.recv_from(&mut recv_buf) {
             Ok((size, peer)) => {
                 println!("echo - proc: {} size: {}", i, size);
+
+                parse_packet(recv_buf);
 
                 let _amt = sock.send_to(&send_buf[..size], &peer);
             },
